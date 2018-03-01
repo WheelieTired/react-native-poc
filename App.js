e8e24ed99f3ea7c55cb6dce5c3cce6e6228aa5bd
cjc734 <cjc4372@rit.edu>
2018-03-01 16:08:11 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/* Temporary file to from tutorial to test out environment */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Mapbox from '@mapbox/react-native-mapbox-gl';
+
+Mapbox.setAccessToken('<your access token here>');
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Street}
+            zoomLevel={15}
+            centerCoordinate={[11.256, 43.770]}
+            style={styles.container}>
+        </Mapbox.MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
