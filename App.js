c22494fd122e3c77a2ae1a9f31585b87be60f86a
sjl9313 <sjl9313@g.rit.edu>
2018-03-20 14:36:28 -0400

    added a basic navbar with no functionality

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import React, {Component} from 'react'
-import { Scene, Router, Actions} from 'react-native-router-flux'
-import { Stylesheet, Text, View, Image} from 'react-native'
+import React, {Component} from 'react';
+import { Stack, Scene, Router, Actions} from 'react-native-router-flux';
+import { Stylesheet, Text, View, Image} from 'react-native';
+
+import CustomNavBarView from "./app/components/CustomNavBarView";
+import CustomNavBar from "./app/components/CustomNavBar";
+import CustomNavBar2 from "./app/components/CustomNavBar2";
 
 import Map from './app/components/map'
+import LoginPage from './app/components/login-page'
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key="root">
-        <Scene key="Map" component={Map} title="Home" />
-      </Scene>
+      <Stack key="root">
+        <Scene key="Map" component={LoginPage} title="Home" />
+
+
+      <Stack key="customNavBar" hideTabBar titleStyle={{alignSelf: 'center'}}>
+                    <Scene
+                      key="customNavBar1"
+                      title="CustomNavBar 1"
+                      navBar={CustomNavBar}
+                      component={CustomNavBarView}
+                      back
+                    />
+                    <Scene
+                      key="customNavBar2"
+                      title="CustomNavBar 2"
+                      navBar={CustomNavBar}
+                      component={CustomNavBarView}
+                      back
+                    />
+                    <Scene
+                      key="customNavBar3"
+                      title="Another CustomNavBar"
+                      navBar={CustomNavBar2}
+                      component={CustomNavBarView}
+                      back
+                    />
+                    <Scene
+                      key="hiddenNavBar"
+                      title="hiddenNavBar"
+                      component={CustomNavBarView}
+                      hideNavBar={true}
+                      back
+                    />
+                  </Stack>
+              </Stack>
     </Router>
   );
 };
