fa191e7894d0debe22c175d5dc162bfe38b06428
Adam Prout <ajp1465@rit.edu>
2018-04-01 13:57:45 -0400

    Working earthquake example

--- app/components/map.js
+++ app/components/map.js
@@ -19,6 +19,49 @@ const styles = StyleSheet.create({
   }
 });
 
+const layerStyles = Mapbox.StyleSheet.create({
+  singlePoint: {
+    circleColor: 'green',
+    circleOpacity: 0.84,
+    circleStrokeWidth: 2,
+    circleStrokeColor: 'white',
+    circleRadius: 5,
+    circlePitchAlignment: 'map',
+  },
+
+  clusteredPoints: {
+    circlePitchAlignment: 'map',
+    circleColor: Mapbox.StyleSheet.source(
+      [
+        [25, 'yellow'],
+        [50, 'red'],
+        [75, 'blue'],
+        [100, 'orange'],
+        [300, 'pink'],
+        [750, 'white'],
+      ],
+      'point_count',
+      Mapbox.InterpolationMode.Exponential,
+    ),
+
+    circleRadius: Mapbox.StyleSheet.source(
+      [[0, 15], [100, 20], [750, 30]],
+      'point_count',
+      Mapbox.InterpolationMode.Exponential,
+    ),
+
+    circleOpacity: 0.84,
+    circleStrokeWidth: 2,
+    circleStrokeColor: 'white',
+  },
+
+  clusterCount: {
+    textField: '{point_count}',
+    textSize: 12,
+    textPitchAlignment: 'map',
+  },
+});
+
 const PointCollection = {
   type: 'FeatureCollection',
   features: [
@@ -44,11 +87,28 @@ export default class Map extends Component<{}> {
           centerCoordinate={[-77.6109, 43.1610]}
           style={styles.container}>
           <Mapbox.ShapeSource
-            id="pointShape"
-            shape={PointCollection}
-            images={{icon: markerIcon}}
-            >
-            <Mapbox.SymbolLayer id="pointSymbol" style={styles.icon} />
+            id="earthquakes"
+            cluster
+            clusterRadius={50}
+            clusterMaxZoom={14}
+            url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson">
+            <Mapbox.SymbolLayer
+              id="pointCount"
+              style={layerStyles.clusterCount}
+            />
+
+            <Mapbox.CircleLayer
+              id="clusteredPoints"
+              belowLayerID="pointCount"
+              filter={['has', 'point_count']}
+              style={layerStyles.clusteredPoints}
+            />
+
+            <Mapbox.CircleLayer
+              id="singlePoint"
+              filter={['!has', 'point_count']}
+              style={layerStyles.singlePoint}
+            />
           </Mapbox.ShapeSource>
         </Mapbox.MapView>
       </View>
