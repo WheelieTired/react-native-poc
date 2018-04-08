4d2c26cf6fbb2fa578c75fa47b8cbf5279c25d1b
Adam Prout <ajp1465@rit.edu>
2018-04-08 00:53:18 -0400

    initial

--- App.js
+++ App.js
@@ -1,78 +1,22 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
-import { Stack, Scene, Tabs, Router, Actions, Drawer} from 'react-native-router-flux';
+import { StackNavigator } from 'react-navigation';
 
 import Registration from './app/components/registration';
-import TabView from './app/components/TabView';
-import TabIcon from './app/components/TabIcon';
-import DrawerContent from './app/components/drawer/DrawerContent';
 import Map from './app/components/map';
-import MenuIcon from './app/images/menu_burger.png';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, backgroundColor: 'transparent', justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tabBarStyle: {
-    backgroundColor: '#eee',
-  },
-  tabBarSelectedItemStyle: {
-    backgroundColor: '#ddd',
-  },
+
+
+const Router = StackNavigator({
+    map: { screen: Map },
+    register: { screen: Registration },
+},
+{
+    initialRouteName: 'map',
 });
 
-const RouterComponent = () => {
-  return (
-    <Router>
-    
-      <Stack key="root">
-
-          <Drawer
-            hideNavBar
-            key="drawer"
-            contentComponent={DrawerContent}
-            drawerImage={MenuIcon}
-            drawerWidth={300}
-          >
-          <Scene hideNavBar panHandlers={null}>
-
-            <Stack
-               key="tab_1"
-               initial
-               title="Tab #1"
-               tabBarLabel="TAB #1"
-               inactiveBackgroundColor="#FFF"
-               activeBackgroundColor="#DDD"
-               icon={TabIcon}
-               navigationBarStyle={{ backgroundColor: 'green' }}
-               titleStyle={{ color: 'white', alignSelf: 'center' }}
-            >
-            <Scene
-               key="tab_1_1"
-               component={Map}
-               title="Home"
-            />
-
-            </Stack>
-
-            <Stack
-              key="tab_2"
-              title="Tab #2"
-              icon={TabIcon}
-            >
-            <Scene
-              key="tab_2_1"
-              component={Registration}
-              title="Register"
-
-            />
-            </Stack>
-          </Scene>
-        </Drawer>
-      </Stack>
-    </Router>
-  );
-};
-
-export default RouterComponent
+export default class App extends Component{
+  render(){
+    return <Router />;
+  }
+}
--- app/components/map.js
+++ app/components/map.js
@@ -1,21 +1,24 @@
 /* Temporary file to from tutorial to test out environment */
-
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 
+
 Mapbox.setAccessToken('pk.eyJ1IjoiYWNhLW1hcGJveCIsImEiOiJjajhkbmNjN2YwcXg0MnhzZnU2dG93NmdqIn0.jEUoPlUBoAsHAZw5GKpgiQ');
 
 export default class Map extends Component<{}> {
+  static navigationOptions={
+    title: 'Map',
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Mapbox.MapView
-            styleURL={"mapbox://styles/aca-mapbox/cj8w8rbjnfwit2rpqudlc4msn"}
-            zoomLevel={1}
-            centerCoordinate={[-77.6109, 43.1610]}
-            style={styles.container}>
-        </Mapbox.MapView>
+          styleURL={"mapbox://styles/aca-mapbox/cj8w8rbjnfwit2rpqudlc4msn"}
+          zoomLevel={1}
+          centerCoordinate={[-77.6109, 43.1610]}
+          style={styles.container}
+        />
       </View>
     );
   }
