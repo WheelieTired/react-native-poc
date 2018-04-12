e0fea66b5575a5ebd27388f84a8c71dd5984ff65
sjl9313 <sjl9313@g.rit.edu>
2018-04-12 17:21:27 -0400

    cleanin up code

--- app/components/map.js
+++ app/components/map.js
@@ -15,29 +15,6 @@ const remotedb = new PouchDB('http://52.91.46.42:5984/points', {
     }
   });
 
-const coordinates = [
-                      [ 46.83973203357882, -114.03678238391878 ],
-                      [ 46.87295902405991, -113.99209678173067 ],
-                                                                              [ 46.87093560040589, -113.99452149868013 ],
-                                                                              [ 46.87096310554335, -113.9945375919342 ],
-                                                                              [ 46.865206279246806, -113.99798859178875 ],
-                                                                              [ 43.08234374942122, -77.67193436622621 ],
-                                                                              [ 46.83984945694505, -114.03257131576538 ],
-                                                                              [ 46.76437226619398, -114.0797245502472 ],
-                                                                              [ 46.87228241874959, -113.99338826537132 ],
-                                                                              [ 46.86556357686214, -113.99877011775972 ],
-                                                                              [ 46.75803867298483, -114.08038437366487 ],
-                                                                              [ 46.75703353476754, -114.08778190612794 ],
-                                                                              [ 46.756276453017826, -114.08323287963869 ],
-                                                                              [ 46.75783470678248, -114.08146798610689 ],
-                                                                              [ 46.86461087822758, -113.99726271629335 ],
-                                                                              [ 46.857618877059664, -114.01904748907329 ],
-                                                                              [ 43.0819284373621, -77.67625808715822 ],
-                                                                              [ 35.8356283888737, -120.65185546875001 ],
-                                                                              [ 39.8145, -99.9946 ],
-                                                                              [ 43.08278256665116, -77.67313599586488 ],
-                                                                              [ 39.8145, -99.9946 ] ]
-
 Mapbox.setAccessToken('pk.eyJ1IjoiYWNhLW1hcGJveCIsImEiOiJjajhkbmNjN2YwcXg0MnhzZnU2dG93NmdqIn0.jEUoPlUBoAsHAZw5GKpgiQ');
 
 
@@ -95,44 +72,6 @@ const layerStyles = Mapbox.StyleSheet.create({
   },
 });
 
-
-const PointCollection = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      id: 'TestPoint',
-      properties: {
-        icon: 'circle-15',
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [-78, 39],
-      },
-    },{
-      type: 'Feature',
-      id: 'TestPoint1',
-      properties: {
-        icon: 'circle-15',
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [-77, 39],
-      },
-    },{
-      type: 'Feature',
-      id: 'TestPoint2',
-      properties: {
-        icon: 'circle-15',
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [-70, 40],
-      },
-    },
-    ]};
-
-
 export default class Map extends Component<{}> {
 
 
@@ -151,15 +90,13 @@ export default class Map extends Component<{}> {
 
 
 
-    createPointCollection(){
+    createPointCollection(that){
 
 
-              //GetPoints().then(function(locations){
-
-                  //console.log("locations:", locations);
 
 
-              }
+
+    }
 
     componentDidMount(){
         var that = this;
@@ -175,52 +112,51 @@ export default class Map extends Component<{}> {
                                                }).on('complete', function (info) {
                                                  console.log("points copied from db");
                                                 console.log("grabbing points from local db");
-                                                              db.allDocs({ startkey: "point", endkey: "point\ufff0" }).then(function (result) {
-                                                                                    var docs = result.rows.map(function (row) {
-                                                                                          return row.doc.location;
-                                                                                    });
-                                                                                    console.log(docs);
-                                                                                    const PointCollection = {
-                                                                                                  type: 'FeatureCollection',
-                                                                                                  features: []
-                                                                                              };
+                                                db.allDocs({ startkey: "point", endkey: "point\ufff0" }).then(function (result) {
+                                                        var docs = result.rows.map(function (row) {
+                                                            return row.doc.location;
+                                                        });
+                                                        console.log(docs);
+                                                        const PointCollection = {
+                                                            type: 'FeatureCollection',
+                                                            features: []
+                                                            };
 
-                                                                                              for(var i=0; i<docs.length; i++){
+                                                        for(var i=0; i<docs.length; i++){
 
 
 
-                                                                                                  if(docs[i]){
-                                                                                                      PointCollection.features.push(
+                                                            if(docs[i]){
+                                                                PointCollection.features.push(
 
-                                                                                                      {
-                                                                                                          type: 'Feature',
-                                                                                                          id: 'TestPoint',
-                                                                                                          properties: {
-                                                                                                              icon: 'circle-15',
-                                                                                                          },
-                                                                                                          geometry: {
-                                                                                                              type: 'Point',
-                                                                                                              coordinates: [docs[i][1], docs[i][0]]
-                                                                                                          }
-                                                                                                      }
+                                                                {
+                                                                    type: 'Feature',
+                                                                    id: 'TestPoint',
+                                                                    properties: {
+                                                                        icon: 'circle-15',
+                                                                    },
+                                                                    geometry: {
+                                                                        type: 'Point',
+                                                                        coordinates: [docs[i][1], docs[i][0]]
+                                                                    }
+                                                                }
 
-                                                                                                      );
-                                                                                                  }
-                                                                                              }
-                                                                                          //console.log("collection",PointCollection);
-                                                                                          //console.log(PointCollection.features)
-                                                                                          that.setState({
-                                                                                            pointsLoaded: true,
-                                                                                            points: PointCollection
-                                                                                          });
-                                                                                    //return docs;
-                                                                                  }).catch(function (err) {
-                                                                                    console.log(err);
-                                                                                  });
+                                                                );
+                                                            }
+                                                        }
 
+                                                        that.setState({
+                                                            pointsLoaded: true,
+                                                            points: PointCollection
+                                                        });
+
+                                                        }).catch(function (err) {
+                                                            console.log(err);
+                                                            });
                                                }).on('error', function (err) {
-                                                 console.log(err.message);
-                                               });
+                                                            console.log(err.message);
+                                                        });
+
 
     }
 
